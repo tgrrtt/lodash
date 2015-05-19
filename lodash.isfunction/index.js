926000e0784e5a2356f3005e4e3c35b1f1d02038
jdalton <john.david.dalton@gmail.com>
2015-05-19 07:27:29 -0700

    Bump to v3.0.8.

--- lodash.isfunction/index.js
+++ lodash.isfunction/index.js
@@ -1,5 +1,5 @@
 /**
- * lodash 3.0.7 (Custom Build) <https://lodash.com/>
+ * lodash 3.0.8 (Custom Build) <https://lodash.com/>
  * Build: `lodash modularize exports="npm" -o ./`
  * Copyright 2012-2016 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
@@ -12,7 +12,7 @@ var funcTag = '[object Function]',
     genTag = '[object GeneratorFunction]';
 
 /** Used for built-in method references. */
-var objectProto = global.Object.prototype;
+var objectProto = Object.prototype;
 
 /**
  * Used to resolve the [`toStringTag`](http://ecma-international.org/ecma-262/6.0/#sec-object.prototype.tostring)
